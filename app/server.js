734e55fbb871ffeea6b9aecc8f1fa2e04dc0f4ef
Von <107226923+ArzuVon@users.noreply.github.com>
2022-09-15 22:27:43 -0400

    Slight touch up

--- app/server.js
+++ app/server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.static('./public'));
 
 app.get('/', (req, res) => {
-  res.status(200).send("Really Boring Home Page");
+  res.status(200).send("A Blank blank Home Page");
 });
 
 app.listen(process.env.PORT, () => console.log(`Listening on Port ${process.env.PORT}`));
